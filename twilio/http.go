3252714e4976ac0db94c312e0685043b3a3cd981
Kevin Burke <kevin@twilio.com>
2013-09-29 00:26:52 -0700

    implement a list interface

--- twilio/http.go
+++ twilio/http.go
@@ -2,7 +2,7 @@ package twilio
 
 import (
 	"encoding/json"
-	//"fmt"
+	"fmt"
 	"github.com/golang/glog"
 	"io/ioutil"
 	"net/http"
@@ -44,39 +44,44 @@ func getFullUri(pathPart string, accountSid string) string {
 }
 
 // Convenience wrapper around MakeRequest
-func (c *Client) GetResource(pathPart string, sid string, v interface{}) error {
+func (c *Client) GetResource(pathPart string, sid string, v interface{}) (http.Response, error) {
 	sidPart := strings.Join([]string{pathPart, sid}, "/")
 	return c.MakeRequest("GET", sidPart, nil, v)
 }
 
-func (c *Client) CreateResource(pathPart string, data url.Values, v interface{}) error {
+func (c *Client) CreateResource(pathPart string, data url.Values, v interface{}) (http.Response, error) {
 	return c.MakeRequest("POST", pathPart, data, v)
 }
 
-func (c *Client) UpdateResource(pathPart string, sid string, data url.Values, v interface{}) error {
+func (c *Client) UpdateResource(pathPart string, sid string, data url.Values, v interface{}) (http.Response, error) {
 	sidPart := strings.Join([]string{pathPart, sid}, "/")
 	return c.MakeRequest("POST", sidPart, nil, v)
 }
 
+func (c *Client) ListResource(pathPart string, data url.Values, v interface{}) (http.Response, error) {
+	return c.MakeRequest("GET", pathPart, data, v)
+}
+
 // Make a request to the Twilio API.
-func (c *Client) MakeRequest(method string, pathPart string, data url.Values, v interface{}) error {
+func (c *Client) MakeRequest(method string, pathPart string, data url.Values, v interface{}) (http.Response, error) {
 	req, err := c.CreateRequest(method, pathPart, data)
 	if err != nil {
 		glog.Errorf("Error creating request", err)
-		return err
+		return http.Response{}, err
 	}
 	resp, err := c.Do(req)
 	if err != nil {
 		glog.Errorf("Error making request", err)
-		return err
+		return http.Response{}, err
 	}
 	defer resp.Body.Close()
 
 	// XXX investigate whether this could overflow with a large response body,
 	// it appears so from reading the ioutil source.
 	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
 	json.Unmarshal(body, &v)
-	return nil
+	return *resp, nil
 }
 
 // Initializes the http request.
@@ -86,6 +91,9 @@ func (c *Client) CreateRequest(method string, pathPart string, data url.Values) 
 		rb = *strings.NewReader(data.Encode())
 	}
 	uri := getFullUri(pathPart, c.AccountSid)
+	if method == "GET" && data != nil {
+		uri = strings.Join([]string{uri, data.Encode()}, "?")
+	}
 	req, err := http.NewRequest(method, uri, &rb)
 	if err != nil {
 		glog.Errorf("Couldn't parse api.twilio.com as a URL. This should not happen", err)
--- twilio/messages.go
+++ twilio/messages.go
@@ -1,6 +1,7 @@
 package twilio
 
 import (
+	//"fmt"
 	"github.com/golang/glog"
 	"net/url"
 )
@@ -19,9 +20,23 @@ type Message struct {
 	Sid   string
 }
 
+type MessagePage struct {
+	NextPageUri string    `json:"next_page_uri"`
+	PageSize    int       `json:"page_size"`
+	Messages    []Message `json:"messages"`
+}
+
+type MessageIterator struct {
+	pos         int
+	messages    []Message
+	nextPageUri string
+	data        url.Values
+	client      *Client
+}
+
 func (m *MessageService) Create(data url.Values) (Message, error) {
 	msg := new(Message)
-	err := m.client.MakeRequest("POST", pathPart, data, msg)
+	_, err := m.client.MakeRequest("POST", pathPart, data, msg)
 	if err != nil {
 		glog.Errorf("Error creating request", err)
 		return *msg, err
@@ -40,4 +55,38 @@ func (m *MessageService) SendMessage(from string, to string, body string, mediaU
 		}
 	}
 	return m.Create(v)
+}
+
+func (m *MessageService) ListMessages(data url.Values) MessageIterator {
+	iterator := new(MessageIterator)
+	if data == nil {
+		iterator.data = url.Values{
+			"Page": {"5000"},
+		}
+	} else {
+		iterator.data = data
+	}
+	iterator.pos = 2000
+	iterator.client = m.client
+	return *iterator
+}
+
+// Returns nil when the list is complete.
+func (m *MessageIterator) Next() (*Message, error) {
+	if m.pos >= len(m.messages) {
+		if m.nextPageUri != "" {
+			m.data.Set("next_page_uri", m.nextPageUri)
+		}
+		var page MessagePage
+		_, err := m.client.ListResource(pathPart, m.data, &page)
+		if err != nil {
+			glog.Errorf("Error creating request", err)
+			return nil, err
+		}
+		m.nextPageUri = page.NextPageUri
+		m.messages = page.Messages
+		m.pos = 0
+	}
+	m.pos += 1
+	return &m.messages[m.pos-1], nil
 }
