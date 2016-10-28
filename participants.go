74bbcb28a483e89765716104dcb495a33647a440
Kevin Burke <kev@inburke.com>
2016-10-27 22:37:06 -0700

    Add a Participant struct

---
+++ participants.go
@@ -0,0 +1,27 @@
+package twilio
+
+// It's difficult to work on this API since Twilio doesn't return Participants
+// after a conference ends.
+//
+// https://github.com/saintpete/logrole/issues/4
+
+type ParticipantService struct {
+	client *Client
+}
+
+type Participant struct {
+	AccountSid             string     `json:"account_sid"`
+	CallSid                string     `json:"call_sid"`
+	ConferenceSid          string     `json:"conference_sid"`
+	DateCreated            TwilioTime `json:"date_created"`
+	DateUpdated            TwilioTime `json:"date_updated"`
+	EndConferenceOnExit    bool       `json:"end_conference_on_exit"`
+	Hold                   bool       `json:"hold"`
+	Muted                  bool       `json:"muted"`
+	StartConferenceOnEnter bool       `json:"start_conference_on_enter"`
+	URI                    string     `json:"uri"`
+}
+
+func participantPathPart(conferenceSid string) string {
+	return "Conferences/" + conferenceSid + "/Participants"
+}
