1ec9298c2e996cc39ea06d6c74c31c86fce36aaf
Kevin Burke <kev@inburke.com>
2017-05-20 15:08:25 -0700

    0.63

--- http.go
+++ http.go
@@ -15,7 +15,7 @@ import (
 )
 
 // The twilio-go version. Run "make release" to bump this number.
-const Version = "0.62"
+const Version = "0.63"
 const userAgent = "twilio-go/" + Version
 
 // The base URL serving the API. Override this for testing.
